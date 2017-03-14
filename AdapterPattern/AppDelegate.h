38aaa63d0c4db26a202605e6253aee50ab988e85
liangjie <liangjiestrong@163.com>
2017-03-14 14:40:44 +0800

    Initial Commit

---
+++ AdapterPattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdapterPattern
+//
+//  Created by liangjie on 2017/3/14.
+//  Copyright © 2017年 liangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
